8e6aa41d0f5c3524607fe8f8e64a1670274c7898
Attah Sunday <attahsundayjr@gmail.com>
2026-08-06 11:58:20 +0100

    Added international formatting to stat values

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -1,5 +1,3 @@
-import CountUp from "./CountUp";
-
 
 type Props = {
     title: string;
@@ -8,20 +6,17 @@ type Props = {
 
 export default function StatCard({ title = "Stat", value = 0 }: Props) {
 
-  return (
-    <div className="p-4 rounded-xl bg-[var(--card-color)] font-[family-name:var(--sora)] flex flex-col gap-2 items-center justify-center w-full h-30 border-1 border-gray-300">
+  const newValue = new Intl.NumberFormat("en", {
+    notation: "compact",
+    maximumFractionDigits: 1
+  }).format(value);
 
-        <div className="font-bold text-blue text-3xl"> 
-          
-          <CountUp
-            from={0}
-            to={value}
-            separator=","
-            direction="up"
-            duration={1}
-            className="count-up-text"
-            delay={0}
-          />
+  return (
+    <div className="p-4 rounded-xl bg-[var(--card-color)] font-[family-name:var(--sora)] flex flex-col gap-2 items-center justify-center w-full border-1 border-gray-300">
+
+        <div className="font-bold text-blue text-3xl">
+
+          {newValue}
 
         </div>
 
