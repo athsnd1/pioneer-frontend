f4160158476796e11769d41fe27dfa22d56d665b
Attah Sunday <attahsundayjr@gmail.com>
2026-08-12 15:50:40 +0100

    Added backend url env variable to prevent cors conflict

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -4,7 +4,7 @@ test("user can register", async ({ page }) => {
 
     await page.goto("/register");
 
-    await page.getByLabel("Email").fill("playuser@user.com");
+    await page.getByLabel("Email").fill(`playwright${Date.now()}@test.com`);
     await page.getByLabel("Password").fill("Playuser1234");
 
     await page.getByRole("button", { name: "Create Account" }).click();
